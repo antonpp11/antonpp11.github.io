e3cc37af690cd959434e0e631ad494b2d3d7c3b7
Anton Pellerin-Petrov <antonpellerinpetrov@gmail.com>
2022-01-26 17:32:27 -0500

    removed unnecessary files

--- generate_resume.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-import time
-
-driver = webdriver.Chrome('~/Downloads/chromedriver')
-# driver = webdriver.Chrome(DRIVER)
-time.sleep(5)
-driver.get('https://antonpp11.github.io/about.html')
-screenshot = driver.save_screenshot('~/Downloads/my_screenshot.png')
-driver.quit()
